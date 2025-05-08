d4576e4ab449911f348c136c68698d671f759dbe
nahom temesgen nadew <nadewnahom8@gmail.com>
2025-05-08 18:16:46 +0300

    min max computer complete

---
+++ assignment2/3_loops/53_max_and_min_trigger0.py
@@ -0,0 +1,23 @@
+'''Write a program that prompts the user for a list of
+numbers, until the user types the number zero, and displays the
+largest and smallest numbers in the list.'''
+
+# Prompt the user for a list of numbers until zero is entered
+numbers = []
+
+while True:
+    number = int(input("Enter a number (0 to stop): "))
+    if number == 0:
+        break
+    numbers.append(number)
+
+minim = numbers[0]
+maxim = =numbers[0]
+
+for i in numbers:
+    if minim > i:
+        minim = i
+    if maxim < i:
+        maxim = i
+print('minimum:', minim)
+print('maximum:', maxim)
